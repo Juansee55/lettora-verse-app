e037608921449aef4362d6c85407fe1a650835dd
Juansee55 <garayjuan832@gmail.com>
2026-05-09 18:27:50 -0300

    Delete android/capacitor.config.ts

--- android/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.juanse.lettora',
-  appName: 'Lettora',
-  webDir: 'dist',
-};
-
-export default config;
