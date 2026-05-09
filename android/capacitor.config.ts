721c62add3196ec9f70242e20a1c0ca12e733fbe
Juansee55 <garayjuan832@gmail.com>
2026-05-09 18:22:54 -0300

    Update capacitor.config.ts

--- android/capacitor.config.ts
+++ android/capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.lettora.app',
+  appId: 'com.juanse.lettora',
   appName: 'Lettora',
   webDir: 'dist',
 };
