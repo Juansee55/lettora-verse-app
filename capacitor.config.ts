b220e267460f798162a561f24226df84c99d84bd
Juansee55 <garayjuan832@gmail.com>
2026-05-09 18:28:30 -0300

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.juanse.lettora',
+  appName: 'Lettora',
+  webDir: 'dist',
+};
+
+export default config;
